901c8ed541daf79554d27392ca5d58335ddb7a99
Adam Schiller <ahs8w@virginia.edu>
2020-12-23 13:15:00 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import os
+import sys
+from setuptools import setup
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a VERSION -m 'version VERSION'")
+    print("  git push --tags")
+    sys.exit()
+
+setup(
+    name="unproject_text",
+    version="0.1.1",
+    author="Matt Zucker",
+    description="Perspective recovery of text using transformed ellipses.",
+    url="https://github.com/mzucker/unproject_text",
+    py_modules=["unproject_text"],
+    install_requires=[
+        "numpy>=1.1.0",
+        "scipy",
+        "pillow",
+        "cv2",
+        "matplotlib"
+    ],
+    entry_points="""
+        [console_scripts]
+        unproject_text=unproject_text:main
+    """,
+)
